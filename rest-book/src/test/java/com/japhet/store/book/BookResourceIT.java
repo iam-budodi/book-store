d947a10f16c2bf54cf82ed37baf64539e631cf8e
Japhet Sebastian <japhet.sebastian01@gmail.com>
2022-09-18 00:51:16 +0300

    fix the failing tests

--- rest-book/src/test/java/com/japhet/store/book/BookResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.japhet.store.book;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class BookResourceIT extends BookResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- rest-number/src/test/java/com/japhet/store/number/NumberResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.japhet.store.number;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NumberResourceIT extends NumberResourceTest {
-    // Execute the same tests but in packaged mode.
-}
